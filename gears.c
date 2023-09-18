546711ad53aefa4e78b691644c6f28c7aced7a5b
VENOM-InstantDeath <mohathepro111@gmail.com>
2023-09-18 01:19:17 -0300

    -

--- gears.c
+++ gears.c
@@ -1,4 +1,3 @@
-#include <bits/types/siginfo_t.h>
 #include <curses.h>
 #include <json-c/json_object.h>
 #include <stdio.h>
@@ -481,11 +480,9 @@ int handleFile(struct TabList *tl, struct Data *data, void* f) {
 		}
 		siginfo_t status;
 		waitid(P_ALL, pid, &status, WEXITED);
-		/*FILE *F = fopen("log", "w");
-		fprintf(F, "wifexited: %d\nwexitstatus: %d\nwifsignaled: %d\nwtermsig: %d\nwifstopped: %d\nwstopsig: %d\nwifcontinued: %d\n",
-				WIFEXITED(status), WEXITSTATUS(status), WIFSIGNALED(status), WTERMSIG(status), WIFSTOPPED(status),
-				WSTOPSIG(status), WIFCONTINUED(status));
-		fclose(F);*/
+		FILE *F = fopen("log", "w");
+		fprintf(F, "si_status: %d\nsi_code: %d", status.si_status, status.si_code);
+		fclose(F);
 		refresh();
 	}
 	return 1;
