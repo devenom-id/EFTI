2575aa6ea6564d885e061e7bb82396bcc610f555
VENOM-InstantDeath <mohathepro111@gmail.com>
2023-09-21 01:17:21 -0300

    Added end to ncread

--- libncread/ncread.c
+++ libncread/ncread.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <ncurses.h>
 #include <stdlib.h>
+#include <string.h>
 #include "ncread.h"
 #include "vector.h"
 
@@ -31,6 +32,75 @@ void clrbox(WINDOW* win, int y, int x, int minlim, int vislim) {
 	}
 }
 
+void right(WINDOW* win, int y, int x, int* p, int* sp, int* minlim, int* vislim, struct string String, int mode) {
+	if (*sp == *vislim) {
+		(*vislim)++;(*minlim)++;
+		clrbox(win,y,x,*minlim,*vislim);
+		int acum1=0;
+		struct string vsliced = vslice(String,*minlim,(*vislim)+1);
+		for (int i=0;i<vsliced.size;i++) {
+			if (mode) mvwaddch(win,y,x+acum1,'*');
+			else mvwaddch(win,y,x+acum1,vsliced.str[i]);
+			acum1++;
+		}
+		wrefresh(win);
+		(*sp)++;return;
+	}
+	(*p)++;
+	(*sp)++;
+
+}
+void left(WINDOW* win, int y, int x, int* p, int* sp, int* minlim, int* vislim, struct string String, int mode) {
+	if (!(*p) && *sp) {
+		(*vislim)--;(*minlim)--;
+		clrbox(win,y,x,*minlim,*vislim);
+		int acum1 = 0;
+		struct string vsliced = vslice(String,*minlim,(*vislim)+1);
+		for (int i=0;i<vsliced.size;i++) {
+			if (mode) mvwaddch(win,y,x+acum1,'*');
+			else mvwaddch(win,y,x+acum1,vsliced.str[i]);
+			acum1++;
+		}
+		wrefresh(win);
+		(*sp)--;return;
+	}
+	(*p)--;
+	(*sp)--;
+}
+void _erase(WINDOW* win, int y, int x, int* p, int* sp, int* minlim, int* vislim, struct string* String, int mode) {
+	string_popat(String, (*sp)-1);
+	clrbox(win,y,x,*minlim,*vislim);
+	if (!p && sp) {(*vislim)--;(*minlim)--;}
+	int acum1 = 0;
+	struct string vsliced = vslice(*String,*minlim,(*vislim)+1);
+	for (int i=0;i<vsliced.size;i++) {
+		if (mode) mvwaddch(win,y,x+acum1,'*');
+		else mvwaddch(win,y,x+acum1,vsliced.str[i]);
+		acum1++;
+	}
+	wrefresh(win);
+	if (!(!(*p) && *sp)) (*p)--;
+	(*sp)--;
+}
+void end(WINDOW* win, int y, int x, int* p, int* sp, int* minlim, int* vislim, int ovislim, struct string String, int mode) {
+	int cpyvslm = ovislim;
+	if (String.size > ovislim) {*vislim=String.size;}
+	else if (String.size < ovislim) {cpyvslm=String.size;}
+	*minlim = String.size-cpyvslm;
+	*p = cpyvslm;
+	*sp = String.size;
+	clrbox(win, y, x, *minlim, *vislim);
+	int acum1 = 0;
+	struct string vsliced = vslice(String,*minlim,(*vislim)+1);
+	for (int i=0;i<vsliced.size;i++) {
+		if (mode) mvwaddch(win,y,x+acum1,'*');
+		else mvwaddch(win,y,x+acum1,vsliced.str[i]);
+		acum1++;
+	}
+	wrefresh(win);
+	return;
+}
+
 int ampsread(WINDOW* win, char** ptr, int y, int x, int vislim, int chlim, int mode) {
 	struct string String;
 	struct string vsliced;
@@ -38,8 +108,13 @@ int ampsread(WINDOW* win, char** ptr, int y, int x, int vislim, int chlim, int m
 	int p = 0;
 	int sp = 0;
 	int minlim = 0;
+	int ovislim = vislim;
 	noecho();
 	clrbox(win,y,x,minlim,vislim);
+	if (*ptr && strlen(*ptr)) {
+		string_add(&String, *ptr);
+		end(win, y, x, &p, &sp, &minlim, &vislim, ovislim, String, mode);
+	}
 	wrefresh(win);
 
 	while (1) {
@@ -47,59 +122,22 @@ int ampsread(WINDOW* win, char** ptr, int y, int x, int vislim, int chlim, int m
 		if (ch == 127) ch = KEY_BACKSPACE;
 		else if (ch == 8) ch = KEY_BACKSPACE;
 		if (ch == 4) {}
-		if (ch == 27) {*ptr = NULL;return 1;}
-		if (ch == '\n') {*ptr = String.str /*listostr(String)*/;return 0;}
-		if (ch == KEY_BACKSPACE) {
+		else if (ch == 27) {*ptr = NULL;return 1;}
+		else if (ch == '\n') {*ptr = String.str;return 0;}
+		else if (ch == KEY_BACKSPACE) {
 			if (!sp) continue;
-			string_popat(&String, sp-1);
-			clrbox(win,y,x,minlim,vislim);
-			if (!p && sp) {vislim--;minlim--;}
-			int acum1 = 0;
-			vsliced = vslice(String,minlim,vislim+1);
-			for (int i=0;i<vsliced.size;i++) {
-				if (mode) mvwaddch(win,y,x+acum1,'*');
-				else mvwaddch(win,y,x+acum1,vsliced.str[i]);
-				acum1++;
-			}
-			wrefresh(win);
-			if (!(!p && sp)) p--;
-			sp--;
+			_erase(win, y, x, &p, &sp, &minlim, &vislim, &String, mode);
+			continue;
 		}
-		if (ch == KEY_RIGHT) {
+		else if (ch == KEY_RIGHT) {
 			if (sp == String.size) continue;
-			if (sp == vislim) {
-				vislim++;minlim++;
-				clrbox(win,y,x,minlim,vislim);
-				int acum1=0;
-				vsliced = vslice(String,minlim,vislim+1);
-				for (int i=0;i<vsliced.size;i++) {
-					if (mode) mvwaddch(win,y,x+acum1,'*');
-					else mvwaddch(win,y,x+acum1,vsliced.str[i]);
-					acum1++;
-				}
-				wrefresh(win);
-				sp++;continue;
-			}
-			p++;
-			sp++;
+			right(win, y, x, &p, &sp, &minlim, &vislim, String, mode);
+			continue;
 		}
-		if (ch == KEY_LEFT) {
+		else if (ch == KEY_LEFT) {
 			if (!sp) continue;
-			if (!p && sp) {
-				vislim--;minlim--;
-				clrbox(win,y,x,minlim,vislim);
-				int acum1 = 0;
-				vsliced = vslice(String,minlim,vislim+1);
-				for (int i=0;i<vsliced.size;i++) {
-					if (mode) mvwaddch(win,y,x+acum1,'*');
-					else mvwaddch(win,y,x+acum1,vsliced.str[i]);
-					acum1++;
-				}
-				wrefresh(win);
-				sp--;continue;
-			}
-			p--;
-			sp--;
+			left(win, y, x, &p, &sp, &minlim, &vislim, String, mode);
+			continue;
 		}
 		if (String.size == chlim) continue;
 		if (ch < 32 || ch > 250) continue;
@@ -114,7 +152,6 @@ int ampsread(WINDOW* win, char** ptr, int y, int x, int vislim, int chlim, int m
 				else mvwaddch(win,y,x+acum1,vsliced.str[i]);
 				acum1++;
 			}
-			wrefresh(win);
 			sp++;continue;
 		}
 		int acum1=0;
@@ -124,7 +161,6 @@ int ampsread(WINDOW* win, char** ptr, int y, int x, int vislim, int chlim, int m
 			else mvwaddch(win,y,x+acum1,vsliced.str[i]);
 			acum1++;
 		}
-		wrefresh(win);
 		sp++;
 		p++;
 	}
